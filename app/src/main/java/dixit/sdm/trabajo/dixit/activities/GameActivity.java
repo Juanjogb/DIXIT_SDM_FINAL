8ceba975040d74a15b74598cdbe01345eefd0564
Carlos Herrera <carloshpiedra@gmail.com>
2018-04-15 12:28:51 +0200

    tabhost puntuaciones agregado

--- app/src/main/java/dixit/sdm/trabajo/dixit/activities/GameActivity.java
+++ app/src/main/java/dixit/sdm/trabajo/dixit/activities/GameActivity.java
@@ -2,6 +2,7 @@ package dixit.sdm.trabajo.dixit.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 import dixit.sdm.trabajo.dixit.R;
 
@@ -11,5 +12,25 @@ public class GameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        //Tabs Control
+        TabHost host = (TabHost) findViewById(R.id.my_tabhost);
+        host.setup();
+
+        //--- Tab1 ---
+        TabHost.TabSpec spec = host.newTabSpec("mitab1");
+        //Tab Indicator specified as Label and Icon
+        spec.setIndicator("Local", getResources().getDrawable(R.drawable.ic_person_outline));
+        spec.setContent(R.id.tab1);
+        host.addTab(spec);
+
+        //--- Tab2 ---
+        spec = host.newTabSpec("mitab2");
+        // Tab Indicator specified as Label and Icon
+        spec.setIndicator("Amigos", getResources().getDrawable(R.drawable.ic_people_outline));
+        spec.setContent(R.id.tab2);
+
+        host.addTab(spec);
+
     }
 }
